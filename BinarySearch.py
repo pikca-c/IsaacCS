df3fec7b0bf095db64f3ce787f037f3de94d451a
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2023-12-05 19:20:48 +0000

    Added samples

--- BinarySearch.py
+++ BinarySearch.py
@@ -14,4 +14,7 @@ def Bsearch(slist,value):
             lb = mid + 1
     return found
 
+#Sample use, returns False
 print(Bsearch([1,2,3,4,5],7))
+#Sample use, returns True
+print(Bsearch([1,2,3,4,5],1))
