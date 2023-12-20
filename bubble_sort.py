709121fdddf92eeb2b9f7d5874396d3e1b426f01
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2023-12-20 14:20:50 +0000

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,2 +1,18 @@
-if list[i] < list[i+1]
-  SWAP
+def bubbleSort(someList):
+  index1 = 1
+  while index1 < len(someList):
+    index1 = index1 + 1
+    index2 = 1
+    while index2 < len(someList):
+      if someList[index2-1] > someList[index2]:
+        print("Ready to bubble ", someList[index2-1])
+        h = input()
+        tmp = someList[index2-1]
+        someList[index2-1] = someList[index2]
+        someList[index2] = tmp
+      print("List looks like this: ", someList)
+      index2  = index2 + 1
+  return someList
+    
+    
+print(bubbleSort([8,7,5,3]))
