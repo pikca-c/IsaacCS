bedd481d2f75b5830e629352d4a801eb38b6cd86
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2024-01-25 16:47:47 +0000

    Update and rename webtech_javascript_lesson_materials to webtech_javascript_lesson_materials.js

---
+++ webtech_javascript_lesson_materials.js
@@ -0,0 +1,16 @@
+https://hackernoon.com/java-vs-javascript-heres-what-you-need-to-know-9b1b708394e2
+
+https://skillcrush.com/blog/what-is-a-javascript-framework/
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>repl.it</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <script src="script.js"></script>
+  </body>
+</html>
